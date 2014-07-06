8e7392e340f8eb70e68a747ab0d557d25c61585a
Phil Cannata <phil.cannata@oracle.com>
2014-07-05 21:52:21 -0500

    Draft 2

--- 01 Basic R/Basic.R
+++ 01 Basic R/Basic.R
@@ -2,5 +2,17 @@
 v <- 211
 v
 "Vectors"
-v <- c(1,2,3,4,5)
-v
+v1 <- c(1,2,3,4,5)
+v1
+v2 <- 1:11
+v2
+v3 <- -5:5
+v3
+"Vector Operations"
+v1 + 2
+sqrt(v2)
+v2 + v3
+length(4:22)
+mean(4:22)
+"Functions"
+apropos("mean")
