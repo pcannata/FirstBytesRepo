6a166488d1643300ce9095be7e32cf291763c2d6
Phil Cannata <phil.cannata@oracle.com>
2014-07-09 15:00:14 -0500

    Draft 2

--- 05 Slidify/slidify setup.R
+++ 05 Slidify/slidify setup.R
@@ -2,7 +2,7 @@ install.packages("devtools")
 library("devtools", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 install_github('slidify', 'ramnathv')
 install_github('slidifyLibraries', 'ramnathv')
-# Make 06 Sldify the current Working Directory
+# Make 06 Slidify the current Working Directory - click on "05 Slidify", then click on More->Set as Working Directory
 library(slidify)
 author("mydeck")
 # cd to mydeck
