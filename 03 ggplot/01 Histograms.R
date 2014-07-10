d6ef022c2684811db9c936cd3428525e54f0c4f8
Phil Cannata <phil.cannata@oracle.com>
2014-07-10 08:27:35 -0500

    Draft 3

--- 03 ggplot/01 Histograms.R
+++ 03 ggplot/01 Histograms.R
@@ -1,3 +1,6 @@
 library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 ggplot(data = diamonds) + geom_histogram(aes(x = carat))
 ggplot(data = diamonds) + geom_density(aes(x = carat, fill = "gray50"))
+ggplot(diamonds, aes(x - carat, y = price)) + geom_point()
+
+
